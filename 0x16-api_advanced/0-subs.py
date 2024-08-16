647fa3bdb0c949f71ff5f09aa35ef74476be479a
Tonyy107 <antonmagdy53@gmail.com>
2024-08-16 16:52:27 +0300

    chore: Refactor 0-subs.py to handle subreddits with no subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,10 +12,13 @@ def number_of_subscribers(subreddit):
         int: The number of subscribers for the subreddit. Returns 0
         if the request fails or the subreddit does not exist.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "api_advanced/1.0.0 (by u/tony107)"}
-    req = requests.get(url, headers, allow_redirects=False)
 
-    if req.status_code >= 300:
+    try:
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        headers = {"User-Agent": "api_advanced/1.0.0 (by u/tony107)"}
+        req = requests.get(url, headers, allow_redirects=False)
+        if req.status_code >= 300:
+            return 0
+        return req.json().get("data").get("subscribers")
+    except requests.RequestException:
         return 0
-    return req.json().get("data").get("subscribers")
