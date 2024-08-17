bdfa2ac4bff84551ff07246c77d886a9d4178ca8
Tonyy107 <antonmagdy53@gmail.com>
2024-08-17 17:02:25 +0300

    Refactor API script to use consistent syntax for accessing nested JSON properties

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-""" API Output Checker """
 import requests
 import sys
 
 if __name__ == "__main__":
 
     user_id = sys.argv[1]
+
     user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
     todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
 
     user_resp = requests.get(user_url).json()
     todos_resp = requests.get(todos_url).json()
 
-    name = user_resp["name"]
+    employee_name = user_resp["name"]
 
     completed_tasks = [task["title"]
                        for task in todos_resp if task["completed"]]
     total_tasks = len(todos_resp)
+    number_of_done_tasks = len(completed_tasks)
 
-    print(f"Employee {name} is done with tasks\
-({len(completed_tasks)}/{total_tasks}):")
+    print(
+        f"Employee {employee_name} is done with tasks\
+({number_of_done_tasks}/{total_tasks}):")
 
     for task in completed_tasks:
         print(f"\t {task}")
