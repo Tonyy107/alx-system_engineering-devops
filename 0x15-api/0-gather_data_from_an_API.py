4c7ab49aae526f27b4bcdae408342a946695f0a1
Tonyy107 <antonmagdy53@gmail.com>
2024-08-17 16:10:22 +0300

    chore: Refactor API script to gather and display completed tasks

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" api hoho """
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    url = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}")
+    ids = url.json()
+    print(f"Employee {ids.get('name')} is done with tasks", end="")
+    url2 = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}")
+    tasks = url2.json()
+    com = []
+    for task in tasks:
+
+        if task.get("completed") == True:
+            com.append(task)
+    print(f"({len(com)}/{len(tasks)}):")
+
+    for task in com:
+        task = task.get("title")
+        print(f"\t{task}")
